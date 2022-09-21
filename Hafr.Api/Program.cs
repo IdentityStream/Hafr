c71f678ec3ae7c6b5b23f39a87301915b8d675b7
Kristian Hellang <kristian@identitystream.com>
2022-09-22 00:17:13 +0200

    Added initial Hafr API

---
+++ Hafr.Api/Program.cs
@@ -0,0 +1,63 @@
+using Hafr.Evaluation;
+using Hafr.Parsing;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("evaluate", (EvaluationModel model) =>
+{
+    if (!Parser.TryParse(model.Template, out var expression, out var error, out var errorPosition))
+    {
+        return Results.Problem(new ProblemDetails
+        {
+            Title = "Failed to parse template.",
+            Detail = error,
+            Status = StatusCodes.Status400BadRequest,
+            Extensions =
+            {
+                { "position", errorPosition }
+            }
+        });
+    }
+
+    try
+    {
+        return Results.Json(expression.EvaluateProperties(model.Data));
+    }
+    catch (TemplateEvaluationException tee)
+    {
+        return Results.Problem(new ProblemDetails
+        {
+            Title = "Failed to evaluate template.",
+            Detail = tee.Message,
+            Status = StatusCodes.Status422UnprocessableEntity,
+            Extensions =
+            {
+                { "position", tee.Position }
+            }
+        });
+    }
+});
+
+app.Run();
+
+public class EvaluationModel
+{
+    public EvaluationModel()
+    {
+        Data = new Dictionary<string, object>();
+    }
+    
+    public string Template { get; set; } = null!;
+
+    public Dictionary<string, object> Data { get; }
+}
